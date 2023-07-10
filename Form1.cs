1f0c0a2b19af0247971b42d76bb509383dd3d50d
C�SAR GEOVANY LANDAVERDE LARIOS <20230355@ricaldone.edu.sv>
2023-07-10 09:38:01 -0600

    xd

--- Form1.cs
+++ Form1.cs
@@ -16,5 +16,10 @@ namespace AppRepositorioJorge
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
     }
 }
