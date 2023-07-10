3ed17ee7ed1bcef276b47fb8fcd3675a07caf835
CSAR GEOVANY LANDAVERDE LARIOS <20230355@ricaldone.edu.sv>
2023-07-10 09:43:49 -0600

    010

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -38,7 +38,7 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(283, 161);
+            this.button1.Location = new System.Drawing.Point(338, 94);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 0;
